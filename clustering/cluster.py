bd4379264fa558a8423f6426b2bfb39df61d3ee8
Dininta Isnurthina <dini.dininta@gmail.com>
2020-10-22 21:02:38 +0700

    Start from scratch

--- clustering/cluster.py
+++
@@ -1,4 +0,0 @@
-class Cluster:
-    def __init__(self, label, documents):
-        self.label = label
-        self.documents = documents
--- preprocessing/casefolder.py
+++
@@ -1,4 +0,0 @@
-class Casefolder:
-    def casefold(self, string):
-        output = string.lower()
-        return output
