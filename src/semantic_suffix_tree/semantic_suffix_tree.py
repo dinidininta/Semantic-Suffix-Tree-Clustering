1f7e5b5ad890cc58d4f6b4d0c6bb2bf7ee060607
Dininta Isnurthina <dini.dininta@gmail.com>
2020-10-26 02:30:43 +0700

    Create implementation for appending first child node to root

--- src/semantic_suffix_tree/semantic_suffix_tree.py
+++ src/semantic_suffix_tree/semantic_suffix_tree.py
@@ -1,9 +1,20 @@
 from src.semantic_suffix_tree.node import Node
+from src.semantic_suffix_tree.suffix_nodes import SuffixNodes
 
 class SemanticSuffixTree:
   def __init__(self, documents):
     self.documents = documents
 
   def build_semantic_suffix_tree(self):
-    root = Node("root", 0)
+    root = None
+    documents_amount = len(self.documents)
+    if(documents_amount > 0):
+      root = Node("root", 0)
+      tree = SuffixNodes()
+      tree.append_node(root)
+      
+      for id_, document in enumerate(self.documents):
+        for word in document:
+          node = Node(word, id_, root)
+          tree.append_node(node)
     return root
--- tests/test_semantic_suffix_tree.py
+++ tests/test_semantic_suffix_tree.py
@@ -2,9 +2,20 @@ import pytest
 from src.semantic_suffix_tree.semantic_suffix_tree import SemanticSuffixTree
 from src.semantic_suffix_tree.node import Node
 
-def test_return_root_node():
-  expected_result = { "name": "root", "index": 0, "parent": None, "children": [] }
-  document = [""]
+def test_return_none():
+  documents = []
+  suffixTree = SemanticSuffixTree(documents)
+  assert suffixTree.build_semantic_suffix_tree() == None, "Should return none when there is no document"
+
+def test_return_first_tree():
+  expected_result = { 
+    "name": "root", 
+    "children": [
+      { "name": "cat", "children": [] }
+    ]
+  }
+  document = ["cat"]
   documents = [document]
   suffixTree = SemanticSuffixTree(documents)
-  assert suffixTree.build_semantic_suffix_tree().print_node() == expected_result, "Should return only root node when there is only empty document"
+
+  assert suffixTree.build_semantic_suffix_tree().print_node() == expected_result, "Should return first tree branch when there is one document with one word"
