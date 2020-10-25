099473fad195f5efe40975748d9b018bef85c989
Dininta Isnurthina <dini.dininta@gmail.com>
2020-10-26 02:01:43 +0700

    Add children to expected result in test_return_root_node

--- tests/test_semantic_suffix_tree.py
+++ tests/test_semantic_suffix_tree.py
@@ -1,8 +1,9 @@
 import pytest
 from src.semantic_suffix_tree.semantic_suffix_tree import SemanticSuffixTree
+from src.semantic_suffix_tree.node import Node
 
 def test_return_root_node():
-  expected_result = { "name": "root", "index": 0, "parent": None }
+  expected_result = { "name": "root", "index": 0, "parent": None, "children": [] }
   document = [""]
   documents = [document]
   suffixTree = SemanticSuffixTree(documents)
